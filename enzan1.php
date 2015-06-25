2b9fda2227e37c9ee69027a8b9feef625675e197
Kato <s.gajirou@gmail.com>
2015-06-25 09:44:53 +0900

    教科書に沿ったphpファイルを大量追加 練習問題回答ファイル追加

---
+++ enzan1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<?php
+$a = 9;
+$b = 3;
+$c = 10;
+$answer1 = $a + $b;
+print " $a と $b を足し算した結果は".$answer1."<br>";
+$answer2 = $a - $b;
+print " $a と $b を引いた結果は".$answer2."<br>";
+$answer3 = $a * $b;
+print " $a と $b をかけた結果は".$answer3."<br>";
+$answer4 = $a / $b;
+print " $a と $b を割った結果は".$answer4."<br>";
+$answer5 = $a % $b;
+print " $a と $b を割った余りは結果は".$answer5."<br>";
+
+
+?>
+</body>
+</html>
---
+++ rensyu1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<?php
+$sei = "加藤";
+$mei = "裕三";
+
+print <<<TABLE
+<table>
+<tr>
+<td>姓</td>
+<td>名</td>
+</tr>
+<tr>
+<td>$sei</td>
+<td>$mei</td>
+</tr>
+</table>
+TABLE;
+
+
+?>
+</body>
+</html>
